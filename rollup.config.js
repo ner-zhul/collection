18ec20b6bd7ecc8dfbfd034873571ca712d8b653
MhcII <snail975@gmail.com>
2018-12-06 11:57:15 +0800

    调整 rollup 配置，将 type is 视为外部依赖

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import resolve from 'rollup-plugin-node-resolve';
 import pkg from './package.json';
 
 export default {
   input: 'src/main.js',
   output: { name: 'collection', file: pkg.main, format: 'es' },
+  plugins: [resolve()],
+  external: ['@ner-zhul/type-is'],
 };
