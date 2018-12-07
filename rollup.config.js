c303226a5cb40e5e2feba184eaf23eb0bcbdfb0e
MhcII <snail975@gmail.com>
2018-12-07 11:06:03 +0800

    更新项目配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import resolve from 'rollup-plugin-node-resolve';
 import pkg from './package.json';
+/* eslint-enable import/no-extraneous-dependencies */
 
 export default {
   input: 'src/main.js',
-  output: { name: 'collection', file: pkg.main, format: 'es' },
+  output: [
+    { name: 'collection', file: pkg.module, format: 'es' },
+    { name: 'collection', file: pkg.main, format: 'cjs' },
+  ],
   plugins: [resolve()],
   external: ['@ner-zhul/type-is'],
 };
